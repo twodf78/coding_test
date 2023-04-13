9bfd1e500692e427474cc0b406370f1ad790c0d9
twodf78 <twodf78@naver.com>
2023-04-13 23:07:17 +0900

    여전히 오류

--- 프로그래머스/lv.2/연속된 부분 수열의 합.js
+++ 프로그래머스/lv.2/연속된 부분 수열의 합.js
@@ -1,25 +1,35 @@
+let sum = 0;
+let start = 0;
+
+const drawback = (k, sequence) => {
+  while (sum > k) {
+    sum -= sequence[start];
+    if (start >= sequence.length - 1) return;
+    start++;
+  }
+};
+
 function solution(sequence, k) {
   var answer = [];
 
   const sumSequence = sequence.map((e1, idx) => [e1, 0]);
   let output = [];
-  let sum = 0;
-  let start = 0;
+
   for (let i = 0; i < sequence.length; i++) {
-    let currentSequence = sumSequence[i];
-    for (let j = i; j < sequence.length; j++) {
-      let currentVal = sequence[j];
-      currentSequence[1] += currentVal;
-      if (currentSequence[1] > k) break;
-      else if (currentSequence[1] < k) continue;
-      else if (currentSequence[1] === k) {
-        if (output.length === 0) output = [i, j];
-        let [previousI, previousJ] = output;
-        let previousMinus = output[1] - output[0];
-        if (previousMinus > j - i) {
-          output = [i, j];
-        }
+    sum += sequence[i];
+
+    if (sum < k) {
+      continue;
+    } else if (sum > k) {
+      drawback(k, sequence);
+    } else if (sum === k) {
+      if (output.length === 0) output = [start, i];
+      let previousMinus = output[1] - output[0];
+      if (previousMinus > i - start) {
+        output = [start, i];
       }
+      start++;
+      sum -= sequence[start];
     }
   }
   return output;
