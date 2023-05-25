c9a6dc53f2f909c3610ef3c0ea752a0affcfcda4
Taehoon Kim <48674812+twodf78@users.noreply.github.com>
2023-05-25 12:25:47 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    var answer = '';
+    if(s.length%2===0){
+        return s.slice( s.length/2-1, s.length/2+1);
+    }
+    else{
+        return s.slice( s.length/2, s.length/2+1);
+    }
+}
