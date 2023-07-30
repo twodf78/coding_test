154d2788b08d52d358ba0a0a5feb53434d2020d3
Taehoon Kim <twodf78@naver.com>
2023-07-30 19:37:47 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -0,0 +1,17 @@
+function solution(clothes) {
+    let answer = 1;
+    const clothesCategory = {};
+    clothes.forEach((e) => {
+        const category = e[1];
+        if (!clothesCategory[category]) {
+            clothesCategory[category] = [e[0]];
+        } else {
+            clothesCategory[category].push(e[0]);
+        }
+    })
+
+    for (const [key, value] of Object.entries(clothesCategory)) {
+        answer *= (value.length + 1);
+    }
+    return answer - 1;
+}
