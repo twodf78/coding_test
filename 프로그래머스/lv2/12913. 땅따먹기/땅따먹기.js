84693aa9a99b4ece6a0b284efa033878355e9690
Taehoon Kim <twodf78@naver.com>
2023-08-03 12:45:58 +0900

    [level 2] Title: 땅따먹기, Time: 97.87 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,13 @@
+function solution(land) {
+    let dp = Array.from(Array(land.length), () => new Array(4).fill(0));
+    dp[0] = [...land[0]]
+    for (let i = 1; i < land.length; i++) {
+        dp[i][0] = land[i][0] + Math.max(dp[i-1][1], dp[i-1][2], dp[i-1][3]);
+        dp[i][1] = land[i][1] + Math.max(dp[i-1][0], dp[i-1][2], dp[i-1][3]);
+        dp[i][2] = land[i][2] + Math.max(dp[i-1][1], dp[i-1][0], dp[i-1][3]);
+        dp[i][3] = land[i][3] + Math.max(dp[i-1][1], dp[i-1][2], dp[i-1][0]);
+    }
+    console.log(dp);
+
+    return Math.max(dp[land.length - 1][0],dp[land.length - 1][1],dp[land.length - 1][2],dp[land.length - 1][3]);
+}
