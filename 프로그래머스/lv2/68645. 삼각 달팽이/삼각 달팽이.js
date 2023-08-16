875769fe9f26ae9f7fe26aeec30bd9b7d34f29a9
Taehoon Kim <twodf78@naver.com>
2023-08-16 12:42:51 +0900

    [level 2] Title: 삼각 달팽이, Time: 104.73 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,40 +1,10 @@
 function solution(n) {
-    var answer = [];
-    let increaseNum = 1;
-    for(let i = 1; i<=n; i++){
-        if(i === n){
-            answer.push(new Array(i).fill(i).map((x, i)=> increaseNum++));
-            break;
-        }
-        answer.push(new Array(i).fill(i).map((x, i)=>i===0 ? increaseNum++ : "/"));
-    }
-    let k = 1;
-    while(k<n-1){
-        k++;
-        if(k%2===0){
-            for(let i = n - (k/2) -1 ; i>=k/2;i--){
-                if(answer[i][answer[i].length - k/2] !== "/")continue;
-                answer[i][answer[i].length - k/2] = increaseNum++;
-            }
-        }
-        else{
-            
-            for(let i = Math.ceil(k/2); i<= n - Math.ceil(k/2); i++){
-                if(i === n - Math.ceil(k/2)){
-                    
-                    for(let j = Math.floor(k/2); j<= answer[i].length - Math.ceil(k/2); j++){
-                        if(answer[i][j] !== "/")continue;
-                        answer[i][j] = increaseNum++;
-                    }
-                    break;
-                }
-                if(answer[i][Math.floor(k/2)] !== "/")continue;
-                answer[i][Math.floor(k/2)] = increaseNum++;;
-            }
-        }
-    }
-
-    return answer.reduce((acc, cur)=> {
-        return acc.concat(cur);
-    }, []);
+  let a = Array(n).fill().map((v, i) => Array(i + 1).fill())
+  let row = -1, col = 0, fill = 0;
+  for (let i = n; i > 0; i -= 3) {
+    for (let j = 0; j < i; j++) a[++row][col] = ++fill
+    for (let j = 0; j < i - 1; j++) a[row][++col] = ++fill
+    for (let j = 0; j < i - 2; j++) a[--row][--col] = ++fill
+  }
+  return a.flat()
 }
