8f2b53740e0a8b6829fcb92cc0ce4f1f18883ae2
Taehoon Kim <twodf78@naver.com>
2023-08-17 12:41:28 +0900

    [unrated] Title: 롤케이크 자르기, Time: 76.23 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,27 +1,26 @@
 function solution(topping) {
+    const a = new Set()
+    const b = {}
+
     let answer = 0;
+    let check = 0
 
-    const rightCount = {};
-    const leftCount = {};
-
-    topping.forEach((x)=>{
-        if(!rightCount[x]) {
-            rightCount[x] = 1;
-            leftCount[x] = 0;
+    for (let i = 0; i < topping.length; i++) {        
+        if (b[topping[i]]) {
+            b[topping[i]]++
+        } else {
+            b[topping[i]] = 1
+            check++            
         }
-        else{
-            rightCount[x]+=1;
-        }
-    })
-    let leftTypeNum = 0;
-    let rightTypeNum = Object.keys(rightCount).length;
-    for(let i = 0; i< topping.length; i++){
-        const current = topping[i];
-        if(leftCount[current] === 0) leftTypeNum += 1;
-        leftCount[current]++;
-        rightCount[current]--;
-        if(rightCount[current] === 0) rightTypeNum -= 1;
-        if(leftTypeNum === rightTypeNum) answer++;
     }
+
+    for (let i = 0; i < topping.length; i++) {
+        a.add(topping[i])
+        b[topping[i]]--
+
+        if (!b[topping[i]]) check--
+        if (a.size === check) answer++
+    }
+
     return answer;
 }
