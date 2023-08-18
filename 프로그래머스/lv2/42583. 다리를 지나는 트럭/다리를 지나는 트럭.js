e782216e99368129c8c880a9197cc7e2872de3e9
Taehoon Kim <twodf78@naver.com>
2023-08-18 13:04:41 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,67 +1,14 @@
 function solution(bridge_length, weight, truck_weights) {
-    let totalTimePassed = 1;
-    const truckMap = truck_weights.map((x)=>{
-        return{
-            weight: x,
-            time: 0,
-        }
-    });
-    let currentTruckOnBridge = [];
-    let totalWeightOnBridge = 0;
-    
-    const whenTimePassed= (passedTime) =>{
-        totalTimePassed+= passedTime;
-        for(let j = 0; j< currentTruckOnBridge.length;j++){
-            const idx = currentTruckOnBridge[j];
-            truckMap[idx].time += passedTime;
-            if(truckMap[idx].time > bridge_length){
-                const certainTruck = truckMap[currentTruckOnBridge.shift()];
-                totalWeightOnBridge -= certainTruck.weight;
-                j--;
-            }
-        }
+  let time = 0, qu = [[0, 0]], weightOnBridge = 0;
+  while (qu.length > 0 || truck_weights.length > 0) {
+    if (qu[0][1] === time) weightOnBridge -= qu.shift()[0];
+    if (weightOnBridge + truck_weights[0] <= weight) {
+      weightOnBridge += truck_weights[0];
+      qu.push([truck_weights.shift(), time + bridge_length]);
+    } else {
+      if (qu[0]) time = qu[0][1] - 1;
     }
-    
-    const putOneTruckOnBridge = (truckIdx, weight) =>{
-        currentTruckOnBridge.push(truckIdx);
-        totalWeightOnBridge+= weight;
-        whenTimePassed(1);
-    }
-    
-    for(let i = 0; i< truck_weights.length; i++){
-        
-        const currentTruck = truckMap[i];
-        
-        if(bridge_length >= currentTruckOnBridge.length && currentTruck.weight + totalWeightOnBridge <= weight){
-            putOneTruckOnBridge(i, currentTruck.weight);
-        }
-        else if(currentTruck.weight + totalWeightOnBridge <= weight){
-            const firstTruck = truckMap[currentTruckOnBridge.shift()];
-            const timeNeededToPassTheFirstTruck = bridge_length - firstTruck.time;
-            totalWeightOnBridge-= firstTruck.weight;
-            whenTimePassed(timeNeededToPassTheFirstTruck);
-            putOneTruckOnBridge(i, currentTruck.weight);
-
-        }else if(bridge_length >= currentTruckOnBridge.length){
-            while(currentTruck.weight + totalWeightOnBridge > weight){
-                const firstTruck = truckMap[currentTruckOnBridge.shift()];
-                const timeNeededToPassTheFirstTruck = bridge_length - firstTruck.time;
-                totalWeightOnBridge-= firstTruck.weight;
-                whenTimePassed(timeNeededToPassTheFirstTruck);
-            }
-            putOneTruckOnBridge(i, currentTruck.weight);
-            
-        }else{
-            while(currentTruck.weight + totalWeightOnBridge > weight || bridge_length < currentTruckOnBridge.length){
-                const firstTruck = truckMap[currentTruckOnBridge.shift()];
-                const timeNeededToPassTheFirstTruck = bridge_length - firstTruck.time;
-                totalWeightOnBridge-= firstTruck.weight;
-                whenTimePassed(timeNeededToPassTheFirstTruck);
-            }
-            putOneTruckOnBridge(i, currentTruck.weight);
-        }
-        
-    }
-
-    return totalTimePassed + bridge_length - truckMap[currentTruckOnBridge.pop()].time;
+    time++;
+  }
+  return time;
 }
