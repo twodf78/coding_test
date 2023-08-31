453f88c40929c2c9c9cfdb5acb7252328d662882
Taehoon Kim <twodf78@naver.com>
2023-08-31 14:04:30 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.07 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,6 +1,7 @@
 function solution(n, wires) {
 var answer = n;
 const wireDict = {};
+    
 wires.forEach((wire)=>{
   if(!wireDict[wire[0]]){
       wireDict[wire[0]] =[];
@@ -12,22 +13,14 @@ wires.forEach((wire)=>{
   wireDict[wire[1]].push(wire[0]);
 
 })
-// console.log(wireDict)
-const wiresArr = Object.entries(wireDict).sort((a, b)=>b[1].length-a[1].length)
-// console.log(wiresArr)
 
 const arrCount = (arr, exclusion, visited)=>{
   visited[arr[0]] = 1;
-  // console.log("exclusion",exclusion)
-  // console.log("visited",visited)
     
   for(let i = 0; i< arr[1].length; i++){
       const current = arr[1][i];
-    // console.log("current",current)
-      // console.log("params",[current.toString(), wireDict[current]])
       if(current == exclusion) continue;
       if(visited[current]) continue;
-
       visited = arrCount([current.toString(), wireDict[current]], exclusion, visited);
   }
   return visited;
@@ -35,19 +28,12 @@ const arrCount = (arr, exclusion, visited)=>{
 
 for(let i = 0 ;i< wires.length; i++){
     const [firstTarget, secondTarget] = wires[i];
-          // console.log("111>>>", firstTarget)
-          // console.log("111params>>>", [firstTarget.toString(), wireDict[firstTarget]])
     
     const firstTargetCount = Object.keys(arrCount([firstTarget.toString(), wireDict[firstTarget]], secondTarget, {})).length; 
-          // console.log("222>>>", secondTarget)
-          // console.log("222params>>>", [secondTarget.toString(), wireDict[secondTarget]])
     
     const secondTargetCount =  Object.keys(arrCount([secondTarget.toString(), wireDict[secondTarget]], firstTarget, {})).length; 
-          // console.log("firstTargetCount :>>",firstTargetCount)
-          // console.log("secondTargetCount :>>",secondTargetCount)
     
     answer = answer < Math.abs(firstTargetCount- secondTargetCount) ? answer : Math.abs(firstTargetCount- secondTargetCount)
-          // console.log("answer :>>",answer)
     
 }
 
