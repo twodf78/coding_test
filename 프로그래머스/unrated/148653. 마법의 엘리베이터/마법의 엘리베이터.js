faeed07aef9e53f679ed662436952979ac24e28b
Taehoon Kim <twodf78@naver.com>
2023-09-07 16:46:18 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,43 +1,20 @@
 function solution(storey) {
     let answer = 0;
     const storeyArr = storey.toString().split("").map(Number).reverse();
-    const addOneToCertainIdx=(idx)=>{
-        if(idx === storeyArr.length){
-           storeyArr.push(0);
-        }
-        storeyArr[idx] =storeyArr[idx] + 1 >= 10 ? 0 : storeyArr[idx] + 1;
-        while(storeyArr[idx] === 0){
-            idx++;
-            if(idx === storeyArr.length ){
-                 storeyArr.push(0);
-            }
+    const addOneToCertainIdx = (idx)=>{
+        do{
+            if(idx === storeyArr.length) storeyArr.push(0);
             storeyArr[idx] =storeyArr[idx] + 1 >= 10 ? 0 : storeyArr[idx] + 1;
-        }
-        return 0;
+        }while(storeyArr[idx++] === 0);
     }
     for(let i = 0; i<= storeyArr.length-1; i++){
         const currentNum = storeyArr[i];
-        
-        if(currentNum === 5 && i!== storey.length - 1){
-            if(storeyArr[i+1] >= 5){
-                answer+= 10 - currentNum;
-                addOneToCertainIdx(i + 1);
-                continue;
-            }
-        }
-        if(currentNum > 5){
+        if(currentNum > 5 || (currentNum === 5 && i!== storey.length - 1 && storeyArr[i+1] >= 5)){
             answer+= 10 - currentNum;
             addOneToCertainIdx(i + 1);
         }else{
             answer+=currentNum;
         }
-        // console.log("currentNum: >>", currentNum)
-        // console.log("answer: >>", answer)
-        // console.log("storeyArr.length: >>", storeyArr.length)
-        // console.log("storeyArr: >>", storeyArr)
-        
     }
-    // const ansArr = storeyArr.reverse().join("");
-    // console.log(ansArr)
     return answer;
 }
