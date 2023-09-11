8408a7726c0b6061289b7b6de2a3f997a9adb9ce
Taehoon Kim <twodf78@naver.com>
2023-09-11 18:35:56 +0900

    [unrated] Title: 점 찍기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.js
@@ -1,20 +1,17 @@
 function solution(k, d) {
     let answer = 1 + Math.floor(d/k);
-    // console.log("answer: >> ", answer)
     const compare = (x, y) =>{
         return x**2 + y **2 <= d**2;
     }
-    let rowCount = answer;
-    let currentX = (answer - 1) * k;
+    let rowCount =answer, currentX = (answer - 1) * k;
     for(let y=k; y<=d; y+=k){
         let currentCount = 0;
         while(!compare(currentX - k*currentCount, y)){
             currentCount++;
         }
         rowCount -= currentCount;
-        if(rowCount < 0) break;
         currentX -= k*currentCount;
-        answer+=rowCount;
+        answer += rowCount;
     }
     return answer;
 }
