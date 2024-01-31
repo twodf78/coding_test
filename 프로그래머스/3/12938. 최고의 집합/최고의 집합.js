853b190c53da37f8d037db6ec88b77062804257f
김태훈 <twodf78@naver.com>
2024-01-31 17:52:26 +0900

    [level 3] Title: 최고의 집합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -1,15 +1,7 @@
 function solution(n, s) {
     if(n > s) return [-1];
-    
-    let minus = 0;
-    while(s%n!==0){
-        s--;
-        minus++;
-    }
-    
-    const answer = [...new Array(n).fill(s/n)];
-
-    for(let i = 0; i< minus; i++){
+    const answer = [...new Array(n).fill(parseInt(s/n))];
+    for(let i = 0; i<s%n; i++){
         answer[i]++;
     }
     return answer.sort((a,b)=>a-b);
