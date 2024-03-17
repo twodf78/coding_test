1d8e05e7ebb793f94c86ea165a1e88af3a0108ee
김태훈 <twodf78@naver.com>
2024-03-17 23:41:36 +0900

    [level 3] Title: 베스트앨범, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,48 +1,31 @@
 function solution(genres, plays) {
-    const dict = [];
-    const genreTime = {};
-    
-    for(let i = 0 ;i< genres.length; i++){
-        const genre = genres[i];
-        const play = plays[i];
-        if(!genreTime[genre]){
-            genreTime[genre] = play;
+    const map = {};
+    for(let i = 0; i< genres.length; i++){
+        const g = genres[i];
+        const p = plays[i];
+        if(map[g]){
+            map[g].push({
+                time: p,
+                idx: i,
+            });
         }else{
-            genreTime[genre] += play;
+            map[g] = [{
+                time: p,
+                idx: i,
+            }];
         }
-        dict.push({
-            idx: i,
-            genre: genres[i],
-            playCount: plays[i],
-        })
     }
-    
-    dict.sort((a, b)=>{
-        if(b.genre === a.genre){
-            return b.playCount - a.playCount;
+    const genreSequence = Object.entries(map).map(([genre, songList])=>{
+        return [genre, songList.reduce((acc, val)=>acc + val.time, 0)];
+    });
+    genreSequence.sort((a,b) => b[1] - a[1]);
+    const answer = [];
+    for(const [genre, _] of genreSequence){
+        map[genre].sort((a, b)=>b.time === a.time ? a.idx - b.idx : b.time - a.time);
+        for(let i = 0 ; i< map[genre].length; i++){
+            if(i===2) break;
+            answer.push(map[genre][i].idx);
         }
-        return genreTime[b.genre] - genreTime[a.genre];
-    })
-    
-    const answer = [dict[0].idx];
-    let previousGenre = dict[0].genre;
-    let genreCount = 1;
-    for(let i = 1; i< dict.length; i++){
-        const currentGenre = dict[i].genre;
-        const currentIdx = dict[i].idx;
-
-
-        if(currentGenre === previousGenre){
-            if(genreCount < 2){
-                answer.push(currentIdx);
-                genreCount++;
-            }
-        }else{
-            genreCount = 1;
-            answer.push(currentIdx);
-        }
-        previousGenre = currentGenre;
     }
-
     return answer;
 }
