b3f9bedde854bf7b5ee264861e1a72c1ce4a8c86
김태훈 <twodf78@naver.com>
2024-03-17 16:51:33 +0900

    [level 2] Title: 모음 사전, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -6,26 +6,13 @@ dict["O"] = 4;
 dict["U"] = 5;
 
 function solution(word) {
-
-    const count = (n) =>{
-        let cnt = 1;
-        let singleAcc = 0;
-        while(n > 1){
-            singleAcc += cnt;
-            cnt *= 5;
-            n--;
-        }
-        return cnt + singleAcc;
-    }
-    
-    const targetList = word.split("");
-    let idx = 5;    
     let answer = 0;
-    while(targetList.length>0){
-        const currentLetter = targetList.shift();
-        const letterOrder = dict[currentLetter];
-        answer += count(idx) * (letterOrder-1);
-        idx--;
+    const wordList = word.split("");
+    let sum = 0;
+    for(let i = 4; i>= 0 ; i--){
+        sum += (5 ** (4-i));
+        if(wordList.length <= i ) continue;
+        answer+= (dict[wordList[i]] - 1)*sum;
     }
     return answer + word.length;
 }
