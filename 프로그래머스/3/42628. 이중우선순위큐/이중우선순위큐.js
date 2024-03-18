528adb07f8b964158a665be69146ee0d1527db1d
김태훈 <twodf78@naver.com>
2024-03-18 19:32:54 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,15 +1,20 @@
 function solution(operations) {
     const queue = [];
-    for (let operation of operations){
-        const [cmd, num] = operation.split(" ");
-        if(cmd === 'I') {
-            queue.push(Number(num))
-        }
-        else {	
-            const findValue = (num === '1' ? Math.max : Math.min)(...queue);
-            const delIdx = queue.indexOf(findValue);
-            queue.splice(delIdx, 1);
-        }
+    for(const c of operations){
+        const [cmd, num] = c.split(" ");
+        if(cmd ==="I"){
+            queue.push(Number(num));
+        }else{
+            if(num === "1"){
+                const findVal= Math.max(...queue);
+                const idx = queue.indexOf(findVal);
+                queue.splice(idx, 1);
+            }else{
+                const findVal= Math.min(...queue);
+                const idx = queue.indexOf(findVal);
+                queue.splice(idx, 1);
+            }
+        } 
     }
-    return queue.length ? [Math.max(...queue), Math.min(...queue)] : [0, 0];
+    return queue.length ? [Math.max(...queue), Math.min(...queue)] : [0,0];
 }
