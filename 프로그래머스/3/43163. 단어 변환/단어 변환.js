39622b5d14c4039fb0d917fc51043ff96bad4889
김태훈 <twodf78@naver.com>
2024-03-20 21:18:29 +0900

    [level 3] Title: 단어 변환, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,38 +1,34 @@
-const isChangeable = (source, target) =>{
-    let different = 0;
-    for(let i = 0; i< target.length; i++){
-        if(source[i] !== target[i]) different++;
-        if(different > 1) return;
+const isValid = (source, target) => {
+    let difCount = 0;
+    for(let i = 0; i<source.length; i++){
+        if(source[i]!==target[i]) difCount++;
+        if(difCount > 1) return false;
     }
-    return different === 1;
+    return difCount === 1;
 }
-
 function solution(begin, target, words) {
-    if(words.indexOf(target) === -1) return 0;
-    
-    const dfs = (sourceList, count) =>{
-        const nextSource = [];
-        while(sourceList.length > 0){
-            const source = sourceList.shift();
+    let count = -1;
+    const visit = Array.from({length: words.length}, _ => false);
+    const bfs = (queue) =>{
+        count++;
+        const newQueue = [];
+        while(queue.length){
+            const word = queue.shift();
+            if(word === target) return count;
             
             for(let i = 0; i< words.length; i++){
-                const word = words[i];
-                if(isChangeable(source, word)){
-                    if(word === target){
-                        return count + 1;
-                    }
-                    words = words.filter((v, idx)=>idx !== i);
-                    nextSource.push(word);
-                    i--;
-                }             
+                if(visit[i]) continue;
+                if(isValid(word, words[i])){
+                    newQueue.push(words[i]);
+                    visit[i] = true;
+                }
             }
         }
-
-        if(nextSource.length > 0){
-            return dfs(nextSource, count + 1);
+        if(newQueue.length){
+            return bfs(newQueue);
+        }else{
+            return 0;
         }
-        return 0;
-    }    
-    
-    return dfs([begin], 0);
+    }
+    return bfs([begin]);
 }
