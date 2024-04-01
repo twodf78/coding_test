39d1e669fad0768611aadcf3da1d8013fb6a72e5
김태훈 <twodf78@naver.com>
2024-04-01 21:54:37 +0900

    [level 1] Title: 둘만의 암호, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,8 +1,8 @@
-
-
 def solution(s, skip, index):
-    alpha = [chr(code) for code in range(97, 123)]
+    alpha = [chr(code) for code in range(97,123)]
     for c in skip:
-        alpha.remove(c)
-    
-    return "".join(map(lambda c: alpha[(alpha.index(c) + index) % len(alpha)], s))
+        alpha.remove(c) 
+    answer = ""
+    for c in s:
+        answer+=alpha[(alpha.index(c) + index) % len(alpha)]
+    return answer
